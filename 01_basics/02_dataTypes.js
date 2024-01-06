ce54923b398c07c852755d04b1ffe99571f3ac8c
Devansh <135505279+just-Devansh@users.noreply.github.com>
2024-01-06 14:16:27 +0000

    added dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// PRIMITIVE DATA TYPES
+// number => 2^53
+// bigint
+// string => "satakshi"
+// boolean => true/false
+// null => standalone value (representation of an empty value)
+// undefined => 
+// symbol => uniqueness (used in React etc.)
+
+// object
+
+console.log(typeof "devansh"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
